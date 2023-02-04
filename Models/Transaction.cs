2380a1d74bbb41676790e0b2b6ad8a18a40b193f
Augusto Maia <augustofmaia@hotmail.com>
2023-02-04 08:09:48 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Transaction.cs
@@ -0,0 +1,29 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppControleFinanceiro.Models
+{
+    class Transaction
+    {
+        [BsonId]
+        public int Id { get; set; }
+
+        public TransactionType Type { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTimeOffset Date { get; set; }
+
+        public decimal Value { get; set; }
+    }
+
+    public enum TransactionType
+    {
+        Income,
+        Expenses
+    }
+}
---
+++ Repositories/ITransactionRepository.cs
@@ -0,0 +1,12 @@
+﻿using AppControleFinanceiro.Models;
+
+namespace AppControleFinanceiro.Repositories
+{
+    internal interface ITransactionRepository
+    {
+        void Add(Transaction transaction);
+        void Delete(Transaction transaction);
+        List<Transaction> GetAll();
+        void Update(Transaction transaction);
+    }
+}
